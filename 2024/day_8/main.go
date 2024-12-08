77607f4b9e31857038d16b2ee087b7d1c5428ee9
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-12-08 23:07:24 +0500

    Cleanup and refactor 2024 Day 8 solution

--- 2024/day_8/main.go
+++ 2024/day_8/main.go
@@ -2,23 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 )
-
-func parseInput(filename string) ([]string, error) {
-	contents, err := os.ReadFile(filename)
-
-	var lines []string
-
-	if err != nil {
-		return lines, err
-	}
-
-	lines = strings.Split(strings.TrimSpace(string(contents)), "\n")
-
-	return lines, nil
-}
 
 func main() {
 	file := "input.txt"
