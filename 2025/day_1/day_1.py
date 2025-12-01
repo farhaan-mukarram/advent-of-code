81cdcac50f83564a55253dc2615cf798064be3fc
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2025-12-01 14:32:15 +0000

    Add solution for 2025 Day 1 (part 1)

---
+++ 2025/day_1/day_1.py
@@ -0,0 +1,50 @@
+from utils import parse_input
+
+
+def rotate(direction: str, value: int, prev_dial_position: int = 50) -> int:
+    dial_position = prev_dial_position
+
+    clicks = value
+
+    while clicks != 0:
+        if direction == "L":
+            if dial_position == 0:
+                dial_position = 99
+            else:
+                dial_position -= 1
+
+        elif direction == "R":
+            if dial_position == 99:
+                dial_position = 0
+
+            else:
+                dial_position += 1
+
+        clicks -= 1
+
+    return dial_position
+
+
+def part_1(rotation_list):
+    zeroes_count = 0
+    dial_position = 50
+
+    for rotation in rotation_list:
+        direction, rotation_value = rotation[:1], int(rotation[1:])
+        dial_position = rotate(
+            direction=direction,
+            prev_dial_position=dial_position,
+            value=rotation_value,
+        )
+
+        if dial_position == 0:
+            zeroes_count += 1
+
+    return zeroes_count
+
+
+if __name__ == "__main__":
+    puzzle_input = parse_input()
+    password = part_1(puzzle_input)
+
+    print(f"The password is {password}")
---
+++ 2025/day_1/utils.py
@@ -0,0 +1,5 @@
+def parse_input():
+    with open("input.txt") as f:
+        data = f.read().splitlines()
+
+        return data
